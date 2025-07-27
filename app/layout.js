565022c7c383eb1cc860e219c5fe60cd5c60ca6d
Kerrie Mariah <kerriemariah@gmail.com>
2025-07-26 20:12:08 -0400

    adding google tag

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@ import Preloader from "@/components/ui/preloader";
 import { cormorantGaramond, openSans } from "./fonts";
 import "./globals.css";
 import Providers from "./providers";
+import Script from "next/script";
 
 export const metadata = {
     title: "Kerrie Mariah - Software Engineer",
@@ -13,6 +14,20 @@ export default function RootLayout({ children }) {
             <body
                 className={`${cormorantGaramond.variable} ${openSans.className}`}
             >
+                {/* Google Analytics */}
+                <Script
+                    src="https://www.googletagmanager.com/gtag/js?id=G-VQTEDTV3G6"
+                    strategy="afterInteractive"
+                />
+                <Script id="google-analytics" strategy="afterInteractive">
+                    {`
+                        window.dataLayer = window.dataLayer || [];
+                        function gtag(){dataLayer.push(arguments);}
+                        gtag('js', new Date());
+                        gtag('config', 'G-VQTEDTV3G6');
+                    `}
+                </Script>
+
                 <Preloader />
                 <Providers>{children}</Providers>
             </body>
